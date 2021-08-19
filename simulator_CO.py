7a965184e284d089633e140dc2782044c712fa8d
Shivam Agrawal <88394791+shivam20124@users.noreply.github.com>
2021-08-20 00:55:36 +0530

    Update simulator_CO.py

--- simulator_CO.py
+++ simulator_CO.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug 18 20:53:39 2021
-
 @author: AKSHITA
 """
 from sys import stdin
@@ -12,11 +11,8 @@ pc=0
 reg_file=[0]*8
 halt=False
 ins_list=[]
-op_dict={"00000":add,"00001":sub,"00010":mov_im,
-         "00011":mov_reg,"00100":load,"00101":store,"00110":mul,"00111":div,
-         "01000":rs,"01001":ls,"01010":xor,"01011":Or,"01100":And,"01101":invert,
-         "01110":cmp,"01111":jmp,"10000":jlt,"10001":jgt,"10010":je,"10011":hlt}
 
+            
 
 
 def load(s):    
@@ -70,12 +66,139 @@ def cmp(s):
     pc+=1
 
 def jmp(s):
+    global pc
     mem_add=int(s[8:],2)
     pc=mem_add
     reg_file[7]=0  
     
+def add(s):
+    r_sto = int(s[7:10])
+    r_1 = int(s[10:13])
+    r_2 = int(s[13:16])
+    sum_reg = reg_file[r_1] + reg_file[r_2]
+    if sum_reg < 65535:
+        reg_file[r_sto] = sum_reg
+        reg_file[7] = 0
+    else:
+        sum_reg =  sum_reg & 65535
+        reg_file[r_sto] = sum_reg
+        reg_file[7] = 8
+    global pc
+    pc +=1
     
-       
+    
+def sub(s):
+    r_sto = int(s[7:10])
+    r_1 = int(s[10:13])
+    r_2 = int(s[13:16])
+    sub_reg = reg_file[r_1] - reg_file[r_2]
+    if sub_reg > 0:
+        reg_file[r_sto] = sub_reg
+        reg_file[7] = 0
+    else:
+        reg_file[r_sto] = sub_reg
+        reg_file[7] = 8
+    global pc
+    pc +=1
+    
+    
+def mov_im(s):
+    r_sto = int(s[5:8])
+    imm = int(s[8:16])
+    reg_file[r_sto] = imm
+    global pc
+    pc +=1
+    reg_file[7] = 0
+  
+    
+def mov_reg(s):
+    r_sto = int(s[10:13])
+    r_1 = int(s[13:16])
+    reg_file[r_sto] = reg_file[r_1]
+    global pc
+    pc +=1
+    reg_file[7] = 0
+    
+    
+def mul(s):
+    r_sto = int(s[7:10])
+    r_1 = int(s[10:13])
+    r_2 = int(s[13:16])
+    pro_reg = reg_file[r_1] * reg_file[r_2]
+    if pro_reg < 65535:
+        reg_file[r_sto] = pro_reg
+        reg_file[7] = 0
+    else:
+        pro_reg =  pro_reg & 65535
+        reg_file[r_sto] = pro_reg
+        reg_file[7] = 8
+    global pc
+    pc +=1
+    
+def div(s):
+    r_1 = int(s[10:13])
+    r_2 = int(s[13:16])
+    reg_file[0] = (reg_file[r_1])//(reg_file[r_2])
+    reg_file[1] = (reg_file[r_1])%(reg_file[r_2])
+    global pc
+    pc +=1
+    reg_file[7] = 0
+    
+def Or(s):
+    r_sto = int(s[7:10])
+    r_1 = int(s[10:13])
+    r_2 = int(s[13:16])
+    or_reg = (reg_file[r_1])|(reg_file[r_2])
+    reg_file[r_sto] = or_reg
+    global pc
+    pc +=1
+    reg_file[7] = 0
+    
+def And(s):
+    r_sto = int(s[7:10])
+    r_1 = int(s[10:13])
+    r_2 = int(s[13:16])
+    and_reg = (reg_file[r_1])&(reg_file[r_2])
+    reg_file[r_sto] = and_reg
+    global pc
+    pc +=1
+    reg_file[7] = 0
+    
+def invert(s):
+    r_sto = int(s[10:13])
+    r_1 = int(s[13:16])
+    not_reg = ~(reg_file[r_1])
+    reg_file[r_sto] = not_reg
+    global pc
+    pc +=1
+    reg_file[7] = 0
+    
+def xor(s):
+    r_sto = int(s[7:10])
+    r_1 = int(s[10:13])
+    r1_val = reg_file[r_1]
+    not_r1 = ~(r1_val)
+    r_2 = int(s[13:16])
+    r2_val = reg_file[r_2]
+    not_r2 = ~(r2_val)
+    xor_reg = ((r1_val)&(not_r2))|((not_r1)&r2_val)
+    reg_file[r_sto] = xor_reg
+    global pc
+    pc +=1
+    reg_file[7] = 0
+    
+    
+
+    
+    
+    
+
+
+op_dict={"00000":add,"00001":sub,"00010":mov_im,
+         "00011":mov_reg,"00100":load,"00101":store,"00110":mul,"00111":div,
+         "01000":rs,"01001":ls,"01010":xor,"01011":Or,"01100":And,"01101":invert,
+         "01110":cmp,"01111":jmp,"10000":jlt,"10001":jgt,"10010":je,"10011":hlt}
+    
     
     
 def convertBin(n,b):
@@ -107,4 +230,3 @@ while(halt==False):
 
 for i in mem:
     print(i)
-   
